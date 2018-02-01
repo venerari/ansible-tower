8e553afce05c4d420a2c559831f9614dc725cbbf
venerari <35533668+venerari@users.noreply.github.com>
2018-02-01 11:15:18 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
+fi
+
+ansible-playbook -i inventory ansible-tower.yml
+
+$lastcommand
